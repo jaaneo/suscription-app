dca8fbb6c469c289d34e9d68f97e09f0b88a53f1
Alejandro <jaaneo@gmail.com>
2023-03-23 16:42:46 -0300

    add EmptyTaskList

---
+++ src/components/screens/TaskApp/components/EmptyTaskList.tsx
@@ -0,0 +1,12 @@
+export default function EmptyTaskList() {
+  return (
+    <section className="py-16 text-center">
+      <h2 className="text-xt mb-2">
+        No hay tareas
+      </h2>
+      <p className="text-sm opacity-50">
+        Agrega una tarea para comenzar
+      </p>
+    </section>
+  )
+}
--- src/components/screens/TaskApp/components/TaskList.tsx
+++ src/components/screens/TaskApp/components/TaskList.tsx
@@ -1,4 +1,5 @@
 import type { Task } from 'src/@types/Task'
+import EmptyTaskList from './EmptyTaskList'
 import TaskItem from './TaskItem'
 
 interface Props {
@@ -9,18 +10,24 @@ interface Props {
 export default function TaskList({ tasks, onTaskRemove }: Props) {
   return (
     <section className="mt-4">
-      <h2 className="text-2xl mb-2">Mis tareas</h2>
-      <ul className="space-y-2">
-        {tasks.map(task => (
-          <TaskItem
-            key={task.id}
-            id={task.id}
-            title={task.title}
-            body={task.body}
-            onRemove={onTaskRemove}
-          />
-        ))}
-      </ul>
+      <h2 className="text-2xl mb-2">
+        Mis tareas
+      </h2>
+      {!tasks.length ? (
+        <EmptyTaskList />
+      ) : (
+        <ul className="space-y-2">
+          {tasks.map(task => (
+            <TaskItem
+              key={task.id}
+              id={task.id}
+              title={task.title}
+              body={task.body}
+              onRemove={onTaskRemove}
+            />
+          ))}
+        </ul>
+      )}
     </section>
   )
 }
